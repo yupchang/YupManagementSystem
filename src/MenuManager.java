6486dec0b185c283f2ef580ef41b2561345fab23
yupchang <rkekqu3310@naver.com>
2022-03-23 22:41:02 +0900

    different case - different block

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,6 +2,31 @@ import java.util.Scanner;
 
 public class MenuManager {
 
+	public static void AddRacket() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Racket's name");
+		String racket_name = input.next();
+		System.out.print("Racket's price");
+		int racket_price = input.nextInt();
+		System.out.print("Racket's company");
+		String racket_company = input.next();
+	}
+	public static void DeleteRacket() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Racket's name");
+		String racket_name = input.next();
+		
+	}
+	public static void EditRacket() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Racket's name");
+		String racket_name = input.next();
+	}
+	public static void ViewRacket() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Racket's name");
+		String racket_name = input.next();
+	}
 	public static void main(String[] args) {
 		int num = 0;
 		Scanner input = new Scanner(System.in);
@@ -13,39 +38,24 @@ public class MenuManager {
 			System.out.println("5. Show a menu");
 			System.out.println("6. Exit");
 			
-			System.out.println("원하는 숫자를 입려하세요.");
+			System.out.println("원하는 숫자를 입력하세요.");
 			num = input.nextInt();
 			
 			switch (num) {
-			case 1 : 
-				System.out.println("추가할 라켓의 이름을 입력하세요.");
-				String racket_name = input.next();
-				System.out.println("라켓의 가격을 입력하세요.");
-				int racket_price = input.nextInt();
-				System.out.println("라켓의 고유번호를 입력하세요");
-				int racket_number = input.nextInt();
+			case 1 : //라켓 추가
+				AddRacket();
 				
 				break;
 			
 			
 			case 2 : //라켓 정보 초기화
-				System.out.println("1번은 삭제, 2번은 뒤로가기");
-				int a = input.nextInt();
-				if (a == 1) {
-					racket_name = "";
-					racket_price = 0;
-					racket_number = 0;
-					break;
-				}
-				else {
-					continue;
-				}
+				DeleteRacket();
 				
 				
 			case 3 : //라켓의 정보 수정
-				
+				EditRacket();
 			case 4 : //라켓 정보 보여주기
-			
+				ViewRacket();
 			case 5 : //메뉴로 돌아가기	
 				continue;
 			case 6 : //프로그램 끝내기
